f06088e1bac7df4da03c8a9f1bce0858670d60a7
Fono Yannick <yfono123@gmail.com>
2023-09-01 19:34:37 +0100

    restore changes

--- cashflowpro/src/main/java/com/cashflowpro/cashflowpro/modele/Utilisateur.java
+++ cashflowpro/src/main/java/com/cashflowpro/cashflowpro/modele/Utilisateur.java
@@ -1,19 +1,20 @@
 package com.cashflowpro.cashflowpro.modele;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
 
 @Entity
 @Table(name = "UTILISATEUR")
+@AllArgsConstructor
+@Setter
+@Getter
+
 public class Utilisateur {
-    public Utilisateur(long matricule, String nom, String prenom, String login, String pwd, Role role) {
-        super();
-        this.nom = nom;
-        this.prenom = prenom;
-        this.login = login;
-        this.pwd = pwd;
-         this.role=role;
-         this.matricule = matricule;
-    }
+
     public Utilisateur(){
 
     }
@@ -36,52 +37,12 @@ public class Utilisateur {
     private String pwd;
     @Column(length = 40)
     private Role role;
+    @Column
+    private float compte;
+    @Column
+    private Date datenaiss;
+    @Column
+    private long telephone;
 
-    public String getNom() {
-        return nom;
-    }
 
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    public String getPrenom() {
-        return prenom;
-    }
-
-    public void setPrenom(String prenom) {
-        this.prenom = prenom;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getPwd() {
-        return pwd;
-    }
-
-    public void setPwd(String pwd) {
-        this.pwd = pwd;
-    }
-
-    public long getMatricule() {
-        return matricule;
-    }
-
-    public void setMatricule(long matricule) {
-        this.matricule = matricule;
-    }
-
-    public Role getRole() {
-        return role;
-    }
-
-    public void setRole(Role role) {
-        this.role = role;
-    }
 }
---
+++ cashflowpro/src/main/java/com/cashflowpro/cashflowpro/repository/PrestationRepository.java
@@ -0,0 +1,7 @@
+package com.cashflowpro.cashflowpro.repository;
+
+import com.cashflowpro.cashflowpro.modele.Prestation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PrestationRepository extends JpaRepository<Prestation, Long> {
+}
